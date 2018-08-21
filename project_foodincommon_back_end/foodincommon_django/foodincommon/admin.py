88a7a6bee50eae5a4adf769bef5dbefef623d478
Jay Escala <Jonathan.J.Escala@gmail.com>
2018-08-21 17:11:02 -0500

    Added Friendship Admin.

--- project_foodincommon_back_end/foodincommon_django/foodincommon/admin.py
+++ project_foodincommon_back_end/foodincommon_django/foodincommon/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Restaurant, RestaurantList
+from .models import Restaurant, RestaurantList, Friendship
 
 admin.site.register(Restaurant)
 admin.site.register(RestaurantList)
+admin.site.register(Friendship)
